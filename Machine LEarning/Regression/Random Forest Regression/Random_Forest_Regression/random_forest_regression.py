708b16b7c8eee8c1347871c03517f86e04fb83b7
SanjotRaibagkar <sanjotraibagkar@gmail.com>
2019-03-29 16:17:36 +0530

    added ML folder

---
+++ Machine LEarning/Regression/Random Forest Regression/Random_Forest_Regression/random_forest_regression.py
@@ -0,0 +1,71 @@
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Random Forest Regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the Random Forest Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Import tools needed for visualization
+from sklearn.tree import export_graphviz
+import pydot
+# Pull out one tree from the forest
+tree = regressor.estimators_[5]
+# Import tools needed for visualization
+from sklearn.tree import export_graphviz
+import pydot
+feature_list = list(dataset.iloc[:, 1:2].columns)
+# Pull out one tree from the forest
+tree = regressor.estimators_[5]
+# Export the image to a dot file
+export_graphviz(tree, out_file = 'tree.dot', feature_names = feature_list, rounded = True, precision = 3)
+# Use dot file to create a graph
+(graph, ) = pydot.graph_from_dot_file('tree.dot')
+# Write graph to a png file
+graph.write_png('tree.png')
+
+
+
+# Get numerical feature importances
+importances = list(regressor.feature_importances_)
+# List of tuples with variable and importance
+feature_importances = [(X, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+# Sort the feature importances by most important first
+feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+# Print out the feature and importances 
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances];
